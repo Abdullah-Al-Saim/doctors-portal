aa541ae6be61a1a21c1687404a4ddfcbbf43cac5
Abdullah-Al-Saim <abdullahalsaim693041@gmail.com>
2022-05-10 03:31:13 +0600

    first commit

---
+++ src/Pages/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h2>Amader somporke jante hoile, 1st a phn a 500 taka dukan. pore data kine. google a sarch koren. khub valo vabe jante parben. amra gorib manus sokti khai.</h2>
+        </div>
+    );
+};
+
+export default About;
