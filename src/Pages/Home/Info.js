300a668c416066e8e6c8206d25ddee29cdde28f2
Abdullah-Al-Saim <abdullahalsaim693041@gmail.com>
2022-05-11 04:11:15 +0600

    first commit

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import InfoCard from './InfoCard';
+import clock from '../../assets/icons/clock.svg'
+import marker from '../../assets/icons/marker.svg'
+import phone from '../../assets/icons/phone.svg'
 
 const Info = () => {
+    const description = [
+        {
+
+        }
+    ]
+
     return (
-        <div class="card lg:card-side bg-base-100 shadow-xl">
-            <figure><img src="https://api.lorem.space/image/album?w=400&h=400" alt="Album" /></figure>
-            <div class="card-body">
-                <h2 class="card-title">New album is released!</h2>
-                <p>Click the button to listen on Spotiwhy app.</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Listen</button>
-                </div>
-            </div>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-12'>
+            <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" description="Lorem Ipsum is simply dummy text of the pri" cardTitle="Opening Hours" img={clock}></InfoCard>
+            <InfoCard bgClass="bg-accent" description="Brooklyn, NY 10036, United States" cardTitle="Visit our location" img={marker}></InfoCard>
+            <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" description="+000 123 456789" cardTitle="Contact us now" img={phone}></InfoCard>
         </div>
     );
 };
