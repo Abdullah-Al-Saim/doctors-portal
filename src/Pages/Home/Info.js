b478749d8061972a53548c5db1dd0edb22b843d6
Abdullah-Al-Saim <abdullahalsaim693041@gmail.com>
2022-05-11 20:41:35 +0600

    first commit

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -5,14 +5,8 @@ import marker from '../../assets/icons/marker.svg'
 import phone from '../../assets/icons/phone.svg'
 
 const Info = () => {
-    const description = [
-        {
-
-        }
-    ]
-
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-12'>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
             <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" description="Lorem Ipsum is simply dummy text of the pri" cardTitle="Opening Hours" img={clock}></InfoCard>
             <InfoCard bgClass="bg-accent" description="Brooklyn, NY 10036, United States" cardTitle="Visit our location" img={marker}></InfoCard>
             <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" description="+000 123 456789" cardTitle="Contact us now" img={phone}></InfoCard>
